0529f5525285558549b999991b12eba7acfeb4a4
guyzis <guyzisling@gmail.com>
2021-10-24 16:34:44 +0300

    added requirments.txt

--- code/dynamical_tools.py
+++ code/dynamical_tools.py
@@ -18,11 +18,11 @@ from scipy.linalg import expm, sinm, cosm
 from scipy import optimize
 from scipy import stats
 import os
-# from sympy.utilities.iterables import multiset_permutations
-# import h5py
-# import sympy as sym
+from sympy.utilities.iterables import multiset_permutations
+import h5py
+import sympy as sym
 import itertools
-# from sympy.core import sympify
+from sympy.core import sympify
 import sys
 
 from utils import *
